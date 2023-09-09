a6cee6fd97be5997a5a2d7940e2e00f35305e25e
Victor Fu <supergothere@gmail.com>
2023-09-09 19:17:13 +0800

    fix: build break

--- src/app/api/version/route.ts
+++ src/app/api/version/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 
 export function GET() {
-  const version = require("@/package.json").version;
+  const version = require("../../../../package.json").version;
   return NextResponse.json({ version });
 }
