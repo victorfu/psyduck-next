cdae9e8b2bc8f4c19a2017c8bd2b5fbffc7bcb4c
Victor Fu <supergothere@gmail.com>
2023-09-16 19:14:16 +0800

    fix: build break

--- src/app/(main)/history/page.tsx
+++ src/app/(main)/history/page.tsx
@@ -3,18 +3,11 @@ import { getQuestions } from "@/lib/actions";
 import { getUserFromHeader } from "@/lib/session-utils";
 import QuestionItem from "@/components/question-item";
 
-export const preload = (uid: string) => {
-  void getQuestions(uid);
-};
 async function HistoryPage() {
   const user = getUserFromHeader();
   const { questions } = await getQuestions(user?.uid);
 
-  return (
-    <div className="container">
-      <QuestionItem questions={questions as Question[]} />
-    </div>
-  );
+  return <QuestionItem questions={questions as Question[]} />;
 }
 
 export default HistoryPage;
