c2d20f6512b4cfb2055c378e6617888c349fcd2f
Victor Fu <supergothere@gmail.com>
2023-10-05 13:50:18 +0800

    fix: adjust wording

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,6 +1,6 @@
 export const METADATA_TITLE = "Psyduck Next";
 export const METADATA_DESCRIPTION =
-  "Psyduck-next is a Next.js 13 boilerplate with firebase authentication and firestore, based on React, TypeScript, and Tailwind CSS.";
+  "Psyduck-Next is a Next.js 13 boilerplate with firebase authentication and firestore, based on React, TypeScript, and Tailwind CSS.";
 
 export const PATHNAME_HOME = "/";
 export const PATHNAME_LOGIN = "/login";
