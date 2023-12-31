6c9dadc5fa9eae4f304189d8c1d14092f4b17a89
Victor Fu <supergothere@gmail.com>
2023-12-31 09:10:38 +0800

    fix: sort users by isAdmin

--- src/app/(main)/administration/users/page.tsx
+++ src/app/(main)/administration/users/page.tsx
@@ -7,35 +7,41 @@ async function UsersPage() {
   const users = await listUsers();
   return (
     <div className="w-full flex flex-col">
-      {users.map((user) => {
-        const isAdmin = user.customClaims?.isAdmin === true;
-        const providerId = user.providerData[0]?.providerId;
-        return (
-          <div
-            className={`border border-gray-300 p-4 rounded ${
-              isAdmin ? "bg-gray-200" : ""
-            } my-2`}
-            key={user.uid}
-          >
-            <div className="mb-1">
-              <strong className="mr-2">Name: </strong>
-              <span className="mr-2">{user.displayName}</span>
+      {users
+        .sort((a, b) => {
+          const isAdminA = a.customClaims?.isAdmin === true ? 1 : 0;
+          const isAdminB = b.customClaims?.isAdmin === true ? 1 : 0;
+          return isAdminB - isAdminA;
+        })
+        .map((user) => {
+          const isAdmin = user.customClaims?.isAdmin === true;
+          const providerId = user.providerData[0]?.providerId;
+          return (
+            <div
+              className={`border border-gray-300 p-4 rounded ${
+                isAdmin ? "bg-gray-200" : ""
+              } my-2`}
+              key={user.uid}
+            >
+              <div className="mb-1">
+                <strong className="mr-2">Name: </strong>
+                <span className="mr-2">{user.displayName}</span>
+              </div>
+              <div className="mb-1 flex">
+                <strong className="mr-2">Admin: </strong>
+                <AdminSwitch uid={user.uid} isAdmin={isAdmin} />
+              </div>
+              <div className="mb-1">
+                <strong className="mr-2">Provider: </strong>
+                {providerIdToName(providerId)}
+              </div>
+              <div className="mb-1">
+                <strong className="mr-2">Email: </strong>
+                {user.email ?? "N/A"}
+              </div>
             </div>
-            <div className="mb-1 flex">
-              <strong className="mr-2">Admin: </strong>
-              <AdminSwitch uid={user.uid} isAdmin={isAdmin} />
-            </div>
-            <div className="mb-1">
-              <strong className="mr-2">Provider: </strong>
-              {providerIdToName(providerId)}
-            </div>
-            <div className="mb-1">
-              <strong className="mr-2">Email: </strong>
-              {user.email ?? "N/A"}
-            </div>
-          </div>
-        );
-      })}
+          );
+        })}
     </div>
   );
 }
