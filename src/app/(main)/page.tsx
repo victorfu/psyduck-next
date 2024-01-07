1efe9f4cfb65f63663f1959f4bac6df5c5f72f00
Victor Fu <supergothere@gmail.com>
2024-01-07 21:07:08 +0800

    fix: build break

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,8 +1,6 @@
 import "server-only";
-import { getUserFromHeader } from "@/lib/session-utils";
 import HospGrid from "@/components/hosp-grid";
 
 export default function Home() {
-  const user = getUserFromHeader();
-  return <HospGrid user={user} />;
+  return <HospGrid />;
 }
