cb45d7f171d83bfc929bfa83574c4b5e5ce50fec
Victor Fu <supergothere@gmail.com>
2024-01-07 23:34:41 +0800

    fix: add login when not logged in

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -10,9 +10,22 @@ import DefaultAvatar from "./ui/default-avatar";
 import Link from "next/link";
 import Logo from "@/components/ui/logo";
 import useNav from "./use-nav";
+import { Button } from "./ui/button";
 
 export default function Nav({ user }: { user?: User }) {
   const { mainNavigation, userNavigation } = useNav(user);
+
+  const LoginButton = () => {
+    if (user) {
+      return null;
+    }
+
+    return (
+      <Button variant="outline" asChild>
+        <Link href="/login">Login</Link>
+      </Button>
+    );
+  };
 
   return (
     <Disclosure as="nav" className="border-b border-gray-200 ">
@@ -43,7 +56,8 @@ export default function Nav({ user }: { user?: User }) {
                 </div>
               </div>
               <div className="hidden sm:ml-6 sm:flex sm:items-center">
-                {/* Profile dropdown */}
+                <LoginButton />
+
                 <Menu as="div" className="relative ml-3">
                   <div>
                     <Menu.Button className="relative flex max-w-xs items-center rounded-full bg-white text-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
@@ -94,7 +108,8 @@ export default function Nav({ user }: { user?: User }) {
                 </Menu>
               </div>
               <div className="-mr-2 flex items-center sm:hidden">
-                {/* Mobile menu button */}
+                <LoginButton />
+
                 <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
                   <span className="absolute -inset-0.5" />
                   <span className="sr-only">Open main menu</span>
